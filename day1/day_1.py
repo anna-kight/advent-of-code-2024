b1af5081dbe7ab078cbf492eadde51263f98ae09
Anna Kight <anna.kight@gmail.com>
2024-12-01 16:59:25 -0700

    Day 1 part 2

--- day1/day_1.py
+++ day1/day_1.py
@@ -4,7 +4,7 @@ right_ids = []
 for x in f:
   loc_ids = x.split('   ')
   left_ids.append(loc_ids[0])
-  right_ids.append(loc_ids[1])
+  right_ids.append(loc_ids[1].strip())
 f.close()
 
 left_ids.sort()
@@ -14,4 +14,10 @@ total_distance = 0
 for i in range(len(left_ids)):
   total_distance += abs(int(left_ids[i]) - int(right_ids[i]))
 
-print(total_distance)
+print("Total distance: " + str(total_distance))
+
+total_similarity_score = 0
+for id in left_ids:
+  similarity_score = right_ids.count(id) * int(id)
+  total_similarity_score += similarity_score
+print("Similarity score: " + str(total_similarity_score))
